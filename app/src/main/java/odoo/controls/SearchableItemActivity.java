3ea9dee7767b598b4cfc43fffd5f26ce9e994b32
Patrick Hobusch <phobusch@elegosoft.com>
2017-08-15 14:27:06 +0200

    [FIX] Allow filtering on barcode in SearchableLiveActivity

--- app/src/main/java/odoo/controls/SearchableItemActivity.java
+++ app/src/main/java/odoo/controls/SearchableItemActivity.java
@@ -237,11 +237,11 @@ public class SearchableItemActivity extends ActionBarActivity implements
             try {
                 ServerDataHelper helper = mRelModel.getServerDataHelper();
                 ODomain domain = new ODomain();
-                domain.add(mRelModel.getDefaultNameColumn(), "ilike", params[0]);
                 if (mRelModel.getDefaultBarcodeColumn() != null) {
                     domain.add("|");
                     domain.add(mRelModel.getDefaultBarcodeColumn(), "ilike", params[0]);
                 }
+                domain.add(mRelModel.getDefaultNameColumn(), "ilike", params[0]);
                 domain.append(liveDomain);
                 return helper.searchRecords(new OdooFields(mRelModel.getColumns()), domain, 10);
             } catch (Exception e) {
